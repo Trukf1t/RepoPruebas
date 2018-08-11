0df048ef2118d3e76e34514526f584b3179c6a0a
Barragán Edgar <barraganedgar@macbook-4.local>
2018-08-11 13:21:00 -0500

    Initial Commit

---
+++ FirstAppGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstAppGame
+//
+//  Created by Barragán Edgar on 11/08/18.
+//  Copyright © 2018 Barragán Edgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
